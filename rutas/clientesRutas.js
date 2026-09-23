f97a4d58fac83f436368d045468fb43fd9c721c9
fviale10 <fviale@coyspu.com.ar>
2026-09-23 18:10:11 -0300

    Add files via upload

---
+++ rutas/clientesRutas.js
@@ -0,0 +1,15 @@
+import { Router } from "express";
+import {
+  getClientes,
+  getClienteById,
+  createCliente,
+  updateCliente,
+  deleteCliente,
+} from "../controladores/clientesControlador.js";
+const router = Router();
+router.get("/", getClientes);
+router.get("/:id", getClienteById);
+router.post("/", createCliente);
+router.put("/:id", updateCliente);
+router.delete("/:id", deleteCliente);
+export default router;
