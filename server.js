7c4bccbbf129574c24bbc57fc3e1a999f0498113
fviale10 <fviale@coyspu.com.ar>
2026-09-26 16:04:52 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import clienteRoutes from "./rutas/clientesRutas.js";
 dotenv.config();
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use("/api/clientes", clienteRoutes);
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
